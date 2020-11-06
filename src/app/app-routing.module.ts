c5fdc2487836609494b5183709379ded1292d4ec
erwan-png <erwan.v@free.fr>
2020-11-06 11:38:29 +0100

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ArtisteComponent } from './artiste/artiste.component';
+import { RechercheComponent } from './recherche/recherche.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'recherche', component: RechercheComponent},
+  { path:'recherche-artiste/:nomArtiste', component: ArtisteComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import {MatButtonModule} from '@angular/material/button'; 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RechercheComponent } from './recherche/recherche.component';
+import { ArtisteComponent } from './artiste/artiste.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RechercheComponent,
+    ArtisteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
