5a9bdb56572aee74e2e10e985a91c52b49982f5f
Benjamin Kermani <benjamin.kermani@gmail.com>
2020-11-10 09:17:35 +0100

    artiste component: updated code and css

--- src/app/artiste/artiste.component.ts
+++ src/app/artiste/artiste.component.ts
@@ -10,33 +10,30 @@ import { HttpClient } from '@angular/common/http';
 export class ArtisteComponent implements OnInit {
 
   nomArtiste: string;
-  url: string = 'http://dbpedia.org/sparql?default-graph-uri=http%3A%2F%2Fdbpedia.org';
+  url = 'http://dbpedia.org/sparql?default-graph-uri=http%3A%2F%2Fdbpedia.org';
   bio: string;
   nom: string;
-  
+
+
   constructor(private route: ActivatedRoute,
               private httpClient: HttpClient) { }
 
   ngOnInit(): void {
-    this.nomArtiste = this.route.snapshot.params['nomArtiste'];
-    //this.nomArtiste = this.nomArtiste.charAt(0).toUpperCase() + this.nomArtiste.substring(1).toLowerCase();
-    const requete: string 
-      ='select distinct ?name ?bio where {'
-      +'?artiste a dbo:MusicalArtist .'
-      +'?artiste foaf:name ?name .'
-      +'?artiste dbo:abstract ?bio .'
-      +'FILTER(?name = "'+this.nomArtiste+'"@en && lang(?bio)="en" ).'
-      +'}';
+    // Initializing variables and request
+    this.nomArtiste = this.route.snapshot.params.nomArtiste;
+    const requete: string =
+      'select distinct ?name ?bio where {'
+      + '?artiste a dbo:MusicalArtist .'
+      + '?artiste foaf:name ?name .'
+      + '?artiste dbo:abstract ?bio .'
+      + 'FILTER(?name = "' + this.nomArtiste + '"@en && lang(?bio)="en" ).'
+      + '}';
 
-    /*console.log('URL : ' + this.url+"&query="+encodeURIComponent(requete)+"&format=json");
-    console.log('Nom : ' + this.nomArtiste)*/
-    this.httpClient.get(this.url+"&query="+encodeURIComponent(requete)+"&format=json")
-    .subscribe(
-      (response) => {
-        //console.log((response as any).results.bindings[0]);
+    // Deserialization JSON into variables
+    this.httpClient.get(this.url + '&query=' + encodeURIComponent(requete) + '&format=json').subscribe((response) => {
         this.bio = (response as any).results.bindings[0].bio.value;
         this.nom = (response as any).results.bindings[0].name.value;
-        console.log(this.nom,this.bio);
+        console.log(this.nom, this.bio);
       }
     );
   }
